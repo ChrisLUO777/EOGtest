a0d34ccb5bc0261f903453adf47bdb0584c997d2
LUOShiqing <2386004278@qq.com>
2019-04-15 21:40:42 -0400

    revise Illusion stimulus

--- Assets/GoogleVR/Demos/Scripts/VortexScript.cs
+++ Assets/GoogleVR/Demos/Scripts/VortexScript.cs
@@ -11,17 +11,17 @@ public class VortexScript : MonoBehaviour {
 	void Start () {
 		intervaltime=0.7f;
 
-		positions [0] = new Vector3 (-3.39f,10.0f,4.13f);
-		positions [1] = new Vector3 (3.44f,10.0f,4.13f);
+		positions [0] = new Vector3 (-7.0f,12.0f,11.0f);
+		positions [1] = new Vector3 (0.0f,12.0f,11.0f);
+		positions [2] = new Vector3 (7.0f,12.0f,11.0f);
 
-		positions [2] = new Vector3 (-11.09f,5.0f,9.32f);
-		positions [3] = new Vector3 (-6.6f,5.0f,13.0f);
-		positions [4] = new Vector3 (-0.02f,5.0f,9.32f);
-		positions [5] = new Vector3 (6.21f,5.0f,13.0f);
-		positions [6] = new Vector3 (11.05f,5.0f,9.32f);
+		positions [3] = new Vector3 (-7.0f,5.0f,11.0f);
+		positions [4] = new Vector3 (0.0f,5.0f,11.0f);
+		positions [5] = new Vector3 (7.0f,5.0f,11.0f);
 
-		positions [7] = new Vector3 (-3.29f,1.0f,4.13f);
-		positions [8] = new Vector3 (3.58f,1.0f,4.13f);
+		positions [6] = new Vector3 (-7.0f,-2.0f,11.0f);
+		positions [7] = new Vector3 (0.0f,-2.0f,11.0f);
+		positions [8] = new Vector3 (7.0f,-2.0f,11.0f);
 
 	}
 
@@ -41,7 +41,7 @@ public class VortexScript : MonoBehaviour {
 		} else {
 			intervaltime -= Time.deltaTime;
 			if (this.GetComponent<Transform> ().name.Equals("Quad"+index)) {
-				this.GetComponent<Transform> ().Translate (0, 0, 10 * Time.deltaTime);
+				this.GetComponent<Transform> ().Translate (0, 0, -5 * Time.deltaTime);
 			}
 		}
 	}
